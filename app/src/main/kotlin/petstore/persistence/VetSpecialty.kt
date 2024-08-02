056fdaa7c2a0dc1a8e469b901ce3b0670669205a
andriesfc <andries.fc@gmail.com>
2024-08-02 07:03:35 +0200

    Add missing field.

--- app/src/main/kotlin/petstore/persistence/VetSpecialty.kt
+++ app/src/main/kotlin/petstore/persistence/VetSpecialty.kt
@@ -7,6 +7,7 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 
 class VetSpecialty(id: EntityID<Long>) : LongEntity(id) {
     object Table : LongIdTable("vet_speciality") {
+        val speciality = reference("specialty_id", Speciality.Table)
     }
     companion object : LongEntityClass<VetSpecialty>(Table)
 }
